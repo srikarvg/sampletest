e2c6225c65a707fc2b6e1de08b89ef45465a8f83
srikarvg <srikarvg@gmail.com>
2015-08-05 16:15:42 +0100

    Initial commit

---
+++ src/main/java/com/srikar/test/sampletest/App.java
@@ -0,0 +1,13 @@
+package com.srikar.test.sampletest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
